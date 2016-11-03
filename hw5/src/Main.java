919ebb39b394dd9e590eac3db87861da435deba3
EasonQiu <easonqiu94@gmail.com>
2016-11-03 11:20:16 -0400

    clean unrelated files

---
+++ hw5/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Student Eason = new Student(001, "Eason");
+        Eason.setAddress("City Station");
+        Eason.setAge(22);
+        Eason.setDepartment("ITWS");
+        Eason.setPhone("110");
+
+        Eason.printStudentInfo();
+    }
+
+}
---
+++ hw5/src/Student.java
@@ -0,0 +1,63 @@
+import com.sun.tools.corba.se.idl.StringGen;
+
+
+
+public class Student {
+    private String name, phone, address, department;
+    private int id, age;
+
+    public Student(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    String getAddress(){
+        return address;
+    }
+
+    protected String getPhone(){
+        return phone;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public String getDepartment(){
+        return department;
+    }
+
+    void setAddress(String address){
+        this.address = address;
+    }
+
+    protected void setPhone(String phone){
+        this.phone = phone;
+    }
+
+    public void setAge(int age){
+        this.age = age;
+    }
+
+    public void setDepartment(String department){
+        this.department = department;
+    }
+
+    public void printStudentInfo(){
+        System.out.println("Name: " + name);
+        System.out.println("ID: " + id);
+        System.out.println("Phone: " + phone);
+        System.out.println("Adress: " + address);
+        System.out.println("Age: " + age);
+        System.out.println("Department: " + department);
+    }
+
+}
